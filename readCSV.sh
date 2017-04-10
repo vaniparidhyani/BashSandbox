089c2b835299a1b120f4cdeaf6454bf3e8462788
Vani <vani.paridhyani@gmail.com>
2017-04-10 10:31:17 +0000

    Added script to read CSV and delete pdfs from AEM author

---
+++ readCSV.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+csv_file=$1
+admin_passwd=$2
+server_url=$3
+
+while read PDF
+do
+	pdf_path=`echo $PDF | sed -e 's/$server_url//g' | tr -d '\r'`
+	curl -s -u admin:$admin_passwd -X POST -F path=$pdf_path -F cmd="deactivate" "$server_url/bin/replicate.json"
+	sleep 10
+	curl -u admin:$admin_passwd -X DELETE "$server_url$pdf_path"
+	sleep 10
+	status=$(curl -s --head -w %{http_code} $server_url$pdf_path -o /dev/null -u admin:$admin_passwd)
+	if [ "$status" == "404" ]
+		then
+		echo "$pdf_path Page deleted successfully"
+	else
+		echo "$pdf_path Page not deleted"
+		exit 1
+	fi
+done < $csv_file
